018ce72f67aa18e47b4f2a1e373fd048901c52f7
Yifan Liu <liuyeefan@outlook.com>
2019-11-19 18:39:43 -0800

    remove unnecessary lines

--- gtk_thread.c
+++ gtk_thread.c
@@ -4,8 +4,8 @@
 
 typedef struct
 {
-  GtkWidget  *window;
-  guint       progress_id;
+	GtkWidget  *window;
+	guint       progress_id;
 } WorkerData;
 
 WorkerData *wd = NULL;
@@ -15,63 +15,53 @@ gdouble fraction = 0.0;
 static gboolean
 worker_finish_in_idle (gpointer data)
 {
-  WorkerData *wd = data;
-  if(wd){
-    /* we're done, stop updating the progress bar */
-    g_source_remove (wd->progress_id);
-    /* and destroy everything */
-    //g_free (wd);
-  }
+	WorkerData *wd = data;
+	if(wd){
+		/* we're done, stop updating the progress bar */
+		g_source_remove (wd->progress_id);
+		/* and destroy everything */
+		g_free (wd);
+  	}
   
   
-  return FALSE; /* stop running */
+  	return FALSE; /* stop running */
 }
 
 static gpointer
 worker (gpointer data)
 {
-  WorkerData *wd = data;
-  
-  /* hard work here */
-  while (fraction < 1){
-    g_usleep (1000000);
-    gfloat temp = gtk_progress_bar_get_fraction (GTK_PROGRESS_BAR (progress_bar));
-  	fraction = (fraction < temp) ? fraction : temp;
+  	WorkerData *wd = data;
+	
+	/* hard work here */
+	while (fraction < 1){
+			g_usleep (1000000);
+			gfloat temp = gtk_progress_bar_get_fraction (GTK_PROGRESS_BAR (progress_bar));
+			fraction = (fraction < temp) ? fraction : temp;
 
-    fraction += 0.1;
-    if (fraction >= 1)
-      fraction = 0;
-    gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress_bar), fraction);
-  }
-  
-  /* we finished working, do something back in the main thread */
-  g_idle_add (worker_finish_in_idle, wd);
-  
-  return NULL;
+			fraction += 0.1;
+			//gdk_threads_add_idle ();
+
+			gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress_bar), fraction);
+	}
+	
+	/* we finished working, do something back in the main thread */
+	g_idle_add (worker_finish_in_idle, wd);
+	
+	return NULL;
 }
 
 static gboolean
 update_progress_in_timeout (gpointer pbar)
 {
-  gtk_progress_bar_pulse (pbar);
-  
-  return TRUE; /* keep running */
+	gtk_progress_bar_pulse (pbar);
+	
+	return TRUE; /* keep running */
 }
 static gboolean
 fill (gpointer   user_data)
 {
     
 	GtkWidget *progress_bar = user_data;
-
-	/*Get the current progress*/
-   // gfloat temp = gtk_progress_bar_get_fraction (GTK_PROGRESS_BAR (progress_bar));
-//	fraction = (fraction < temp) ? fraction : temp;
-
-	/*Increase the bar by 10% each time this function is called*/
-	//fraction += 0.1;
-
-	/*Fill in the bar with the new fraction*/
-	gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress_bar), fraction);
 
 	/*Ensures that the fraction stays below 1.0*/
 	if (fraction < 1.0) 
@@ -88,13 +78,10 @@ button_clicked_handler (GtkWidget  *button,
     
     GThread    *thread;
     if (wd){
-        g_idle_add (worker_finish_in_idle, wd);
+        g_source_remove(wd->progress_id);
     }
-    wd = (WorkerData*)g_malloc (sizeof WorkerData);
+    wd = (WorkerData*)g_malloc (sizeof (WorkerData));
     wd->window = data;
-        
-    /* add a timeout that will update the progress bar every 100ms */
-    wd->progress_id = g_timeout_add (500, fill, progress_bar);
         
     /* run the time-consuming operation in a separate thread */
     thread = g_thread_new ("worker", worker, wd);
